a2ac8738a4f9b6376435d4a98ce5fa6e4ed326a3
soukaina203 <soukainamourabit9@gmail.com>
2024-07-03 18:51:13 +0100

    landing page: in good progress

--- src/app/modules/landing/landing-page.component.ts
+++ src/app/modules/landing/landing-page.component.ts
@@ -6,11 +6,14 @@ import { AboutComponent } from './about/about.component';
 import { TopRecipesComponent } from './top-recipes/top-recipes.component';
 import { FlavorsComponent } from './flavors/flavors.component';
 import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { CtaComponent } from './cta/cta.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule,HeaderComponent,HomeComponent,AboutComponent,TopRecipesComponent,FlavorsComponent,TestimonialsComponent],
+  imports: [CommonModule,HeaderComponent,HomeComponent,AboutComponent,TopRecipesComponent,FlavorsComponent,
+    CtaComponent
+    ,TestimonialsComponent],
   templateUrl: './landing-page.component.html',
 })
 export class LandingPageComponent {
