c1f54f91d62e50c517fd2de5592d5d69f67c2c17
soukaina203 <soukainamourabit9@gmail.com>
2025-08-21 23:51:43 +0100

    url pb

--- src/app/modules/landing/top-recipes/top-recipes.component.ts
+++ src/app/modules/landing/top-recipes/top-recipes.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Observable } from 'rxjs';
 import { Recipe } from 'app/models/Recipe';
 import { UowService } from 'app/services/uow.service';
+import { environment } from 'environments/environment';
 
 @Component({
   selector: 'app-top-recipes',
@@ -14,6 +15,7 @@ import { UowService } from 'app/services/uow.service';
 export class TopRecipesComponent {
     private uow =inject(UowService)
     recipes$ : Observable<Recipe[]>
+    recipePicture=environment.url
     ngOnInit(): void {
         this.recipes$ = this.uow.recipes.getAll();
 
